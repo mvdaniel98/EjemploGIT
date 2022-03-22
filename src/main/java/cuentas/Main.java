54e5b25bb7d9f73e1b2be13f69284ba44c238966
1DAW24 <1DAW24@INFORMATICA-124.DOMDPTOINFJMPP.INF>
2022-03-22 16:28:38 +0100

    update

---
+++ src/main/java/cuentas/Main.java
@@ -0,0 +1,39 @@
+/*
+
+ */
+package cuentas;
+
+/**
+ *
+ * @author 1DAW24
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        operativa_cuenta(1200);
+    }
+
+    private static void operativa_cuenta(float cantidad) {
+        CCuenta Cuenta1;
+        double saldoActual;
+        
+
+        Cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = Cuenta1.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+
+        try {
+            Cuenta1.retirar(2300);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            Cuenta1.ingresar(695);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+}
+    
+
